540f927152808afaeccfc047f7301c38e1ac93d6
syaifulghifari <syaiful.ghifari12@gmail.com>
2022-10-17 23:12:51 +0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <div className='h-10 w-full bg-slate-500 sticky top-0'>ini navbar</div>
+    </div>
+  );
+}
+
+export default Navbar;

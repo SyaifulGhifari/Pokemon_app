499419fb6f0a64454ff734a9a50eb921801deed2
syaifulghifari <syaiful.ghifari12@gmail.com>
2022-10-18 09:26:30 +0700

    add img and card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Navbar() {
   return (
     <div>
-      <div className='h-10 w-full bg-slate-500 sticky top-0'>ini navbar</div>
+      <div className='h-10 w-full bg-transparent sticky top-0 flex justify-end'>
+        ini navbar
+      </div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import PokeCard from '../components/PokeCard';
 
 function Home() {
+  const coba = [1, 2, 3, 4, 5];
   return (
-    <div>
-      <Navbar />
-      <div>cekk</div>
+    <div className='bg-blue-400'>
+      <div className='w-2/3 bg-slate-400 h-screen mx-auto'>
+        <Navbar />
+        <div className='grid px-10 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          {coba.map(() => {
+            return <PokeCard />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
